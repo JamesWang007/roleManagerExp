02ce8cba8bcda56d18837e4b6eb9787bd4034330
JamesWang007 <wanghaiyang20@gmail.com>
2015-08-05 15:12:42 -0700

    Create sample_Roles.cs

---
+++ sample_Roles.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Commencement_Mvc_Data.DTO
+{
+    public class Roles
+    {
+        public virtual string RoleId { get; set; }
+        public virtual string Emplid { get; set; }
+        public virtual string RoleName { get; set;}
+    }
+}
